2ccdb89a9d6c9003573a94e9633716782c956d0f
Kevin <kevin.esposado@gmail.com>
2023-02-02 23:48:29 +0800

    added

--- server.js
+++ server.js
@@ -9,31 +9,33 @@ const credentialsRoutes = require('./routes/credentials')
 const departmentRoutes = require('./routes/department')
 
 
+
 /**EXPRESS APP 
  * express thingy
 */
-const app = express() 
+const app = express()
 // const corsOptions = {
 //         origin: 'https://coop-2af5d.web.app/',
 //         credentials: true,
 //     };
 
-app.use((req,res,next)=>{
-        res.setHeader('Access-Control-Allow-Origin','*');
-        res.setHeader('Access-Control-Allow-Methods','GET,POST,PUT,PATCH,DELETE');
-        res.setHeader('Access-Control-Allow-Methods','Content-Type','Authorization');
+app.use((req, res, next) => {
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
+        res.setHeader('Access-Control-Allow-Methods', 'Content-Type', 'Authorization');
         res.setHeader("Access-Control-Allow-Credentials", "true");
         res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
-        next(); 
-    })
+        next();
+})
+app.use(cors({ origin: true })); // enable origin cors
 /**MIDDLEWARE
  * it will run all the time whenever there are request
  * it is like a global function that will run always whenever there are request detected(ex: GET, POST, etc)
  */
 // app.use(cors(corsOptions()));
-app.use((req, res, next)=>{
-     console.log('I am detecting a request - MIDDLEWARE')
-     next();
+app.use((req, res, next) => {
+        console.log('I am detecting a request - MIDDLEWARE')
+        next();
 })
 app.use(express.json())
 
@@ -48,13 +50,13 @@ app.use('/api/departments', departmentRoutes)
 
 /**CONNECT TO DB */
 mongoose.connect(process.env.MONGO_URI)
-        .then(()=>{
+        .then(() => {
                 //listen request: 4000, callback function(the console log) to fire if successfully listen to 4000
-                app.listen(process.env.PORT || 5000, ()=> {
-                console.log(`Connected to DB and Listening to port ${process.env.PORT}`)
+                app.listen(process.env.PORT || 5000, () => {
+                        console.log(`Connected to DB and Listening to port ${process.env.PORT}`)
                 })
 
         })
-        .catch((error) =>{
+        .catch((error) => {
                 console.log(error)
         })
