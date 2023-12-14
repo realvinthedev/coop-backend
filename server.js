bc904f231cc7f2ba28c3cfee4f5ccbe4b34ecbf1
Kevin <kevin.esposado@gmail.com>
2023-12-14 23:21:21 +0800

    updated

--- server.js
+++ server.js
@@ -30,6 +30,7 @@ app.use((req, res, next) => {
         res.setHeader('Access-Control-Allow-Origin', '*');
         res.header("Access-Control-Allow-Origin", "http://localhost:3000");
         res.header("Access-Control-Allow-Origin", "https://coop-2af5d.web.app");
+        res.header("Access-Control-Allow-Origin","https://chrome.browserless.io/screenshot") // new. remove it if not working
         res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
         res.setHeader('Access-Control-Allow-Methods', 'Content-Type', 'Authorization');
         res.setHeader("Access-Control-Allow-Credentials", "true");
