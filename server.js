1602031d4d22de9b2bbb3a4330e7d027b9936ea1
Kevin <kevin.esposado@gmail.com>
2024-02-19 22:23:29 +0800

    updated

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const savingsRoutes = require('./routes/savings')
 const payslipRoutes = require('./routes/payslip')
 const drawerRoutes = require('./routes/drawer')
 const customerRoutes = require('./routes/customer')
-
+const creditRoutes = require('./routes/credit')
 
 
 const app = express()
@@ -67,6 +67,7 @@ app.use('/api/savings', savingsRoutes)
 app.use('/api/payslip', payslipRoutes)
 app.use('/api/drawer', drawerRoutes)
 app.use('/api/customer', customerRoutes)
+app.use('/api/credit', creditRoutes)
 /**CONNECT TO DB */
 mongoose.connect(process.env.MONGO_URI)
         .then(() => {
